3db77e0b1fe269f59354af1c8bc6f65f5d3186f7
Johannes Schmidt <johannes.schmidt@1und1.de>
2024-01-04 20:26:07 +0100

    add icons and merge-button

--- src/components/upload/ListUploadedFiles.jsx
+++ src/components/upload/ListUploadedFiles.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import UploadedFilesContext from '../../context/UploadedFilesContext'
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd'
+import { FaTrashCan, FaEllipsisVertical } from 'react-icons/fa6'
 
 function ListUploadedFiles() {
   const { uploadedFiles, setUploadedFiles } = useContext(UploadedFilesContext)
@@ -18,29 +19,46 @@ function ListUploadedFiles() {
     setUploadedFiles(reorder(uploadedFiles, source.index, destination.index))
   }
 
+  const handleMergeRequest = () => {
+    uploadedFiles.map((file) => console.log(file))
+  }
+
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId='droppable'>
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            {uploadedFiles.map((file, index) => (
-              <Draggable key={file.id} index={index} draggableId={file.id}>
-                {(provided, snapshot) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    {file.filename} - {file.size} bytes{' '}
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId='droppable'>
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {uploadedFiles.map((file, index) => (
+                <Draggable key={file.id} index={index} draggableId={file.id}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className='flex mb-1 mt-1'
+                    >
+                      <FaEllipsisVertical />
+                      {file.filename} - {file.size} bytes{' '}
+                      <FaTrashCan className='ml-1' />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+
+      <div>
+        {uploadedFiles.length > 0 && (
+          <button className='btn btn-active' onClick={handleMergeRequest}>
+            Merge
+          </button>
         )}
-      </Droppable>
-    </DragDropContext>
+      </div>
+    </>
   )
 }
 
