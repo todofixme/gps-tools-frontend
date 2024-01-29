908eeb635edc9f18f3b01b9458db08747ccafdc3
Johannes Schmidt <johannes.schmidt@1und1.de>
2024-01-29 18:47:52 +0100

    set baseURL of backend-server

--- src/components/http-common.js
+++ src/components/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:7001',
+  // baseURL: 'http://localhost:7001',
+  baseURL: 'https://gps-backend.tigerflanke.de',
   headers: {
     'Content-type': 'application/json',
   },
