fcd73ec387c445403dca48ea9444edf3873051d1
Johannes Schmidt <johannes.schmidt@1und1.de>
2024-01-29 19:52:39 +0100

    add server-settings

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   plugins: [react()],
+  base: '/',
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  },
 })
