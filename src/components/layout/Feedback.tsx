17187b307e0a5203ad9663784cf40af816cf5926
Johannes Schmidt <devshred@tigerflanke.de>
2024-02-15 22:18:33 +0000

    feat: use react-daisyui to style alerts

--- src/components/layout/Feedback.tsx
+++ src/components/layout/Feedback.tsx
@@ -10,13 +10,26 @@ function Feedback() {
 
   return (
     state !== null && (
-      <Alert status={state.type}>
-        <Button color='ghost' onClick={() => handleRemoveFeedback()}>
-          X
-        </Button>
-        <div className='w-full flex-row justify-between gap-2'>
-          <h3>{state.message}</h3>
-        </div>
+      <Alert
+        status={state.type}
+        icon={
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='stroke-current shrink-0 h-6 w-6'
+            fill='none'
+            viewBox='0 0 24 24'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+            />
+          </svg>
+        }
+      >
+        <span>{state.message}</span>
+        <Button size='sm' onClick={() => handleRemoveFeedback()}>Close</Button>
       </Alert>
     )
   )
