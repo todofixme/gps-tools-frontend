825f5e3f9315805ee2ce0a6165f06ee8c1a0a921
Johannes Schmidt <devshred@tigerflanke.de>
2024-04-21 20:27:42 +0200

    feat(site): describe project at about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -27,8 +27,107 @@ const About = () => {
     <>
       <h1 className='text-6xl mb-4'>GPS Tools</h1>
       <p className='mb-4 text-2xl font-light'>An app dealing with GPS files.</p>
-      <p className='text-lg'>Frontend Version: {import.meta.env.PACKAGE_VERSION}</p>
-      <p className='text-lg'>Backend Version: {backendVersion.app}</p>
+      <p className='text-lg'>
+        <a
+          href='https://github.com/devshred/gps-tools-frontend'
+          className='hover:underline'
+          target='_blank'
+        >
+          Frontend Version
+        </a>
+        :{' '}
+        <a
+          href='https://github.com/devshred/gps-tools-frontend/releases'
+          className='hover:underline'
+          target='_blank'
+        >
+          {import.meta.env.PACKAGE_VERSION}
+        </a>
+      </p>
+      <p className='text-lg'>
+        <a
+          href='https://github.com/devshred/gps-tools-backend'
+          className='hover:underline'
+          target='_blank'
+        >
+          Backend Version
+        </a>
+        :{' '}
+        <a
+          href='https://github.com/devshred/gps-tools-backend/releases'
+          className='hover:underline'
+          target='_blank'
+        >
+          {backendVersion.app}
+        </a>
+      </p>
+      <p className='text-lg mt-4'>
+        This app was created to test some technologies. These are:
+      </p>
+      <ul className='text-lg mt-4'>
+        <li>
+          <a
+            href='https://react.dev/'
+            className='hover:underline'
+            target='_blank'
+          >
+            React
+          </a>
+          ,{' '}
+          <a
+            href='https://tailwindcss.com/'
+            className='hover:underline'
+            target='_blank'
+          >
+            Tailwind CSS
+          </a>
+          ,{' '}
+          <a
+            href='https://daisyui.com/'
+            className='hover:underline'
+            target='_blank'
+          >
+            daisyUI
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://kotlinlang.org/'
+            className='hover:underline'
+            target='_blank'
+          >
+            Kotlin
+          </a>
+          ,{' '}
+          <a
+            href='https://spring.io/'
+            className='hover:underline'
+            target='_blank'
+          >
+            Spring
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://leafletjs.com/'
+            className='hover:underline'
+            target='_blank'
+          >
+            Leaflet
+          </a>
+          ,{' '}
+          <a
+            href='https://photon.komoot.io/'
+            className='hover:underline'
+            target='_blank'
+          >
+            photon
+          </a>
+        </li>
+      </ul>
+      <p className='text-lg mt-4'>
+        Contact: gps minus tools ät devshred dot org
+      </p>
     </>
   )
 }
