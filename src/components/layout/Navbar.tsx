420536dc971ef57e949c4f94c7cc3ac298467314
Johannes Schmidt <devshred@tigerflanke.de>
2024-05-23 07:37:55 +0200

    fix(layout): don't use container and viewport

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -15,27 +15,23 @@ const Navbar: React.FC<Props> = ({ title = DEFAULT_TITLE }) => {
   const { getMessage } = useLanguage()
 
   return (
-    <nav className="navbar mb-12 shadow-lg base-300 text-base-content">
-      <div className="container mx-auto">
-        <div className="flex-none px-2 mx-2">
-          <FaGlobe className="inline pr-2 text-3xl" />
-          <Link to="/" className="text-lg font-bold align-middle">
-            {title}
-          </Link>
-        </div>
+    <nav className="navbar flex flex-row mb-12 shadow-lg base-300 text-base-content">
+      <div className="flex-none px-2 mx-2">
+        <FaGlobe className="inline pr-2 text-3xl" />
+        <Link to="/" className="text-lg font-bold align-middle">
+          {title}
+        </Link>
+      </div>
 
-        <div className="flex-1 px-2 mx-2">
-          <div className="flex justify-end">
-            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
-              {getMessage('home')}
-            </Link>
-            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
-              {getMessage('about')}
-            </Link>
-            <LanguageSelector />
-            <ThemeSwitcher />
-          </div>
-        </div>
+      <div className="flex-1 px-2 mx-2 justify-end">
+        <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+          {getMessage('home')}
+        </Link>
+        <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+          {getMessage('about')}
+        </Link>
+        <LanguageSelector />
+        <ThemeSwitcher />
       </div>
     </nav>
   )
