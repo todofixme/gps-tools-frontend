fbe0fd07633104294eacc98458f143d255ce5a5a
Johannes Schmidt <devshred@tigerflanke.de>
2024-06-05 22:01:58 +0200

    feat(layout): improve visibility on mobile devices

--- src/components/layout/NavMobile.tsx
+++ src/components/layout/NavMobile.tsx
@@ -11,10 +11,10 @@ const NavMobile = () => {
   useClickAway(ref, () => setOpen(false))
 
   return (
-    <div ref={ref} className="md:hidden flex-1 justify-end mr-6 text-white">
+    <div ref={ref} className="md:hidden ml-2 mr-6 text-white">
       <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
       {isOpen && (
-        <div className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20 z-10">
+        <div className="bg-footer fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 border-b border-b-white/20 z-10">
           <NavMobileButton label="home" linkTo="/" icon={<FaHome />} setOpen={setOpen} />
           <NavMobileButton label="faq" linkTo="/faq" icon={<FaQuestion />} setOpen={setOpen} />
           <NavMobileButton label="about" linkTo="/about" icon={<FaLightbulb />} setOpen={setOpen} />
